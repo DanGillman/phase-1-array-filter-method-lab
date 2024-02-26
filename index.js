b2e585f1a52e7b926a4782c4cb93ac6ec9a42bab
dangillman <dangillman93@gmail.com>
2024-02-26 12:27:16 -0500

    added content to index.js, passed tests.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution here
+function findMatching(driversNames, searchString) {
+    const searchLowerCase = searchString.toLowerCase();
+    const matchingDrivers = driversNames.filter(driver => driver.toLowerCase() === searchLowerCase);
+    return matchingDrivers;
+  }
+
+function fuzzyMatch(driversNames, query) {
+  const queryLowerCase = query.toLowerCase();
+
+  const matchingDrivers = driversNames.filter(driver => {
+    const driverLowerCase = driver.toLowerCase();
+    return driverLowerCase.startsWith(queryLowerCase);
+  });
+
+  return matchingDrivers;
+}
+function matchName(driverObjects, query) {
+    const queryLowerCase = query.toLowerCase();
+  
+    const matchingDrivers = driverObjects.filter(driver => {
+      const driverNameLowerCase = driver.name.toLowerCase();
+      return driverNameLowerCase === queryLowerCase;
+    });
+  
+    return matchingDrivers;
+  }
